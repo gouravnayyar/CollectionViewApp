996fded676f995b56cdd3319dd5bea1c284f3432
Gourav <GNayyar@vmware.com>
2018-12-22 08:45:32 +0530

    Initial commit

---
+++ CollectionViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewApp
+//
+//  Created by Gourav Nayyar on 22/12/18.
+//  Copyright © 2018 Gourav Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
